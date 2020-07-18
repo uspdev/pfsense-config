01d869e834799980c5b761b4cacdd681dc655faa
Masaki Kawabata Neto <kawabata@usp.br>
2020-07-17 22:25:14 -0300

    charset

--- public/logout/index.php
+++ public/logout/index.php
@@ -4,7 +4,17 @@ require_once __DIR__ . '/../../config.php';
 unset($_SESSION);
 session_destroy();
 ?>
-<h1>Configurador do firewall do SET</h1>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Firewall do SET</title>
+    <meta charset="UTF-8">
+</head>
+<body>
+    <h1>Configurador do firewall do SET</h1>
 
-Você saiu do sistema.<br>
-Volte à <a href="../">página inicial</a> para entrar novamente.
+    Você saiu do sistema.<br>
+    Volte à <a href="../">página inicial</a> para entrar novamente.
+</body>
+
+</html>
