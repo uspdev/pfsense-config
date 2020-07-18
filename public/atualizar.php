59f7c653654a51b64eb1fe19acb65c8282788e66
Masaki Kawabata Neto <kawabata@usp.br>
2020-07-17 22:15:34 -0300

    gerando log

--- public/atualizar.php
+++ public/atualizar.php
@@ -7,12 +7,16 @@ if (!isset($_SESSION['user'])) {
 }
 
 $ip = $_SERVER['REMOTE_ADDR'];
-$usr = $_SESSION['user']['loginUsuario'];
+$codpes = $_SESSION['user']['loginUsuario'];
 
 exec("ssh $pfsense_ssh pfSsh.php playback updateNat $codpes", $fw);
 $fw = json_decode($fw[0], true);
 
 foreach ($fw as $rule) {
     exec("ssh $pfsense_ssh pfSsh.php playback updateNat $codpes $ip", $fw);
+    $log = date('Y-m-d H:i:s') . '; ' . $codpes .
+        '; target=' . $rule['destination']['address'] . ':' . $rule['destination']['port'] .
+        '; old_ip=' . $rule['source']['address'] . '; new_ip=' . $ip;
+    file_put_contents(__DIR__ . '/../log/update.log', $log . PHP_EOL, FILE_APPEND | LOCK_EX);
 }
 header('Location:index.php');
