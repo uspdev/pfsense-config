d5ae7e962e72f4d826329cd160a676b3d0871a23
Masaki Kawabata Neto <kawabata@usp.br>
2020-07-20 19:44:24 -0300

    compatibilizando com php antigo

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -2,15 +2,21 @@
 session_start();
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__.'/../');
+if (version_compare(phpversion(), '7.0', '<')) {
+    // php version isn't high enough
+    $dotenv = Dotenv\Dotenv::create(__DIR__ . '/../');
+} else {
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+}
 $dotenv->load();
 
-function is_auth() {
+function is_auth()
+{
     if (!isset($_SESSION['user'])) {
         header('Location:login/');
         exit;
     }
-    
+
     $ret = $_SESSION['user'];
     $ret['nome'] = $_SESSION['user']['nomeUsuario'];
     $ret['codpes'] = $_SESSION['user']['loginUsuario'];
