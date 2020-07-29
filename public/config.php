e4789f9f59a5bae9dcb0c1e862d96e632a9f91da
Masaki Kawabata Neto <kawabata@usp.br>
2020-07-29 14:12:49 -0300

    mostrando config para admin

--- public/config.php
+++ public/config.php
@@ -1,6 +1,8 @@
 <?php
 require_once __DIR__ . '/../app/bootstrap.php';
+
 use Uspdev\Pfconfig\Pfsense;
+use raelgc\view\Template;
 
 $usr = is_auth();
 
@@ -8,6 +10,30 @@ if (!$usr->admin) {
     exit;
 }
 
-$config = Pfsense::obterConfig();
+$tpl = new Template('../tpl/config.html');
 
-echo json_encode($config);
+$config = Pfsense::obterConfig(true);
+
+$nat = $config['nat']['rule'];
+
+foreach ($nat as $rule) {
+    //print_r(json_decode(json_encode($rule)));exit;
+    $rule = json_decode(json_encode($rule));
+    if (empty($rule->source->address)) {
+        $rule->source->address = '';
+    }
+    if (empty($rule->{'associated-rule-id'})) {
+        $rule->fw = '';
+    } else {
+        $rule->fw = 'fw ok';
+    }
+    preg_match('/\(.*?\)/', $rule->descr, $matches);
+    $rule->date = substr($matches[0], 1, -1);
+
+    $tpl->rule = $rule;
+    $tpl->block('block_nat_rule');
+}
+
+$tpl->show();
+//echo json_encode($nat);
+//echo json_encode($config);
